2ba6e2412a8bc303e9f1229c78695e3dfdb80f26
Andres-Soler <andres.solers@uniagustiniana.edu.co>
2026-05-08 19:12:35 +0000

    Deploys

--- app/uno/page.tsx
+++ app/uno/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Link href="/uno">Botón Uno</Link>
         <Link href="/dos">Botón Dos</Link>
 
-        <Props nombre='soy el gatomeowmiaugod' />
+        <Props nombre='soy el gatomeowmiaugod + god de los gatos gods puro god' />
 
     </div>
   );
